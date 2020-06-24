52b56564dcce6b1a94db10141984168e50632dd0
Bao Dien <baodien1405@gmail.com>
2020-06-24 11:18:21 +0700

    Update TodoForm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'I love Easy Frontend! 😍 ' },
+    { id: 2, title: 'We love Easy Frontend! 🥰 ' },
+    { id: 3, title: 'They love Easy Frontend! 🚀 ' },
+  ]);
+
+  function handleTodoList(todo) {
+    console.log(todo);
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
+  function handleTodoFormSubmit(formValues) {
+    console.log('Form submit: ', formValues);
+    // add new todo to current todo list
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues,
+    };
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
-      <h1>Welcome to React Hook!</h1>
+      <h1>React hooks - TodoList</h1>
+
+      <TodoForm onSubmit={handleTodoFormSubmit} />
+      <TodoList todos={todoList} onTodoClick={handleTodoList} />
     </div>
   );
 }
